2b35422372033cc2cb499e3b4bf744945a34c502
Lakshan-K <lakshan67@hotmail.com>
2023-12-23 19:11:43 -0500

    Add import re

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np 
+import re
   
 from tensorflow.keras.preprocessing.text import Tokenizer 
 from tensorflow.keras.preprocessing.sequence import pad_sequences 
