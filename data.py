2f49597c63a392f09e64d21aff05b00c5b177135
varshigak <varshigak@gmail.com>
2023-12-24 22:57:38 -0500

    Implement prediction functionality and include preprocessed_data.pickle and tokenizer.json to optimize runtime

--- data.py
+++ data.py
@@ -14,6 +14,10 @@ from keras.layers import Embedding, LSTM, Dense, Dropout  # For specifying layer
 from sklearn.model_selection import train_test_split  # For splitting the dataset into train and test sets
 from sklearn import preprocessing                  # For data preprocessing
 from keras.callbacks import EarlyStopping, ModelCheckpoint  # For setting up callbacks for model training
+from keras.models import load_model                # For loading the Keras model
+import json 
+import os
+import pickle
 
 # Define the text cleaning function 
 def clean_text(text):
@@ -33,6 +37,8 @@ def clean_text(text):
     text = re.sub(r'in', '', text)
     # Remove alphanumeric words (words containing digits)
     text = re.sub(r'\w*\d\w*', '', text)
+    # Replace newline characters with spaces
+    text = text.replace('\n', ' ')
     # Return the cleaned text
     return text
 
@@ -42,62 +48,109 @@ true_csv_path = "news_datasets/True.csv"
 # Read the CSV files
 fake_data = pd.read_csv(fake_csv_path)
 true_data = pd.read_csv(true_csv_path)
-fake_data.head() 
-true_data.head()
 
 # Assign labels to dataset
 fake_data["label"] = 0 
 true_data['label'] = 1
 
 # Merge datasets
-data_merge = pd.concat([fake_data, true_data], axis = 0) 
-data_merge.head(10)
+data_merge = pd.concat([fake_data, true_data], axis=0) 
 
 # Drop columns
-data = data_merge.drop(['title', 'subject', 'date'], axis = 1)
+data = data_merge.drop(['title', 'subject', 'date'], axis=1)
 
-# Clean the text data
-data['text'] = data['text'].apply(clean_text)
+# Define a function to preprocess the data
+def preprocess_data(data, max_words, maxlen):
+    # Clean the text data
+    data['text'] = data['text'].apply(clean_text)
+    
+    # Text Tokenization
+    tokenizer = Tokenizer(num_words=max_words)
+    tokenizer.fit_on_texts(data['text'])
+    
+    # Convert text to sequences of integers
+    sequences = tokenizer.texts_to_sequences(data['text'])
+    
+    # Padding sequences (adjust maxlen as needed)
+    X = pad_sequences(sequences, maxlen=maxlen)
+    
+    # Labels
+    y = data['label'].values
+    
+    return X, y, tokenizer
 
-# Text Tokenization
+# Text Tokenization and Padding (only if preprocessed data does not exist)
 max_words = 10000  # You can adjust this based on your vocabulary size
-tokenizer = Tokenizer(num_words=max_words)
-tokenizer.fit_on_texts(data['text'])
-
-# Convert text to sequences of integers
-sequences = tokenizer.texts_to_sequences(data['text'])
-
-# Padding sequences (adjust maxlen as needed)
 maxlen = 200  # adjust this based on desired sequence length
-X = pad_sequences(sequences, maxlen=maxlen)
 
-# Labels
-y = data['label'].values
+if os.path.exists('preprocessed_data.pickle'):
+    with open('preprocessed_data.pickle', 'rb') as f:
+        loaded_data = pickle.load(f)
+    X, y, tokenizer, maxlen = loaded_data
+else:
+    X, y, tokenizer = preprocess_data(data, max_words, maxlen)
+    
+    with open('preprocessed_data.pickle', 'wb') as f:
+        pickle.dump((X, y, tokenizer, maxlen), f)
 
 # Splitting the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create an empty sequential model
-model = Sequential()
-# Add an embedding layer for processing text data
-model.add(Embedding(input_dim=max_words, output_dim=128, input_length=maxlen))
-# Add a type of neural layer called LSTM to process sequences
-model.add(LSTM(128, return_sequences=True))
-# Add another LSTM layer for deeper processing
-model.add(LSTM(64))
-# Add a layer with ReLU activation function for more complex patterns
-model.add(Dense(64, activation='relu'))
-# Add dropout to prevent overfitting (a technique to improve model's generalization)
-model.add(Dropout(0.5))
-# Add the final layer for binary classification (0 or 1) using sigmoid activation
-model.add(Dense(1, activation='sigmoid'))
+# Check if 'best_model.h5' file exists
+if os.path.exists('best_model.h5'):
+    # Load the pre-trained model
+    model = load_model('best_model.h5')
+else:
+    # Create an empty sequential model
+    model = Sequential()
+    # Add an embedding layer for processing text data
+    model.add(Embedding(input_dim=max_words, output_dim=128, input_length=maxlen))
+    # Add a type of neural layer called LSTM to process sequences
+    model.add(LSTM(128, return_sequences=True))
+    # Add another LSTM layer for deeper processing
+    model.add(LSTM(64))
+    # Add a layer with ReLU activation function for more complex patterns
+    model.add(Dense(64, activation='relu'))
+    # Add dropout to prevent overfitting (a technique to improve model's generalization)
+    model.add(Dropout(0.5))
+    # Add the final layer for binary classification (0 or 1) using sigmoid activation
+    model.add(Dense(1, activation='sigmoid'))
+    # Compile the model with settings for training
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    # Train the model using the training data with 5 training cycles (epochs)
+    # and update model's parameters using batches of 64 data points
+    history = model.fit(X_train, y_train, epochs=5, batch_size=64, validation_split=0.2)
+    model.save('best_model.h5')
 
-# Compile the model with settings for training
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# Modify the classify_text function to accept and classify multiple lines of text
+def classify_text(input_text):
+    # Clean the input text
+    cleaned_text = clean_text(input_text)
+    
+    # Tokenize and pad the input text
+    sequences = tokenizer.texts_to_sequences([cleaned_text])
+    padded_sequences = pad_sequences(sequences, maxlen=maxlen)
+    
+    # Make predictions for each sequence
+    predictions = model.predict(padded_sequences)
+    
+    # Interpret the predictions for each sequence
+    results = []
+    for prediction in predictions:
+        if prediction < 0.5:
+            results.append("The text is likely to be fake.")
+        else:
+            results.append("The text is likely to be real.")
+    
+    return results
 
-# Train the model using the training data with 5 training cycles (epochs)
-# and update model's parameters using batches of 64 data points
-history = model.fit(X_train, y_train, epochs=5, batch_size=64, validation_split=0.2)
+# Collect input
+user_input = input("Enter a piece of text to classify: ")
+# Classify the input text
+results = classify_text(user_input)
+# Print the results
+for result in results:
+    print(result)
 
 # Evaluate the model on the test data
 loss, accuracy = model.evaluate(X_test, y_test)
